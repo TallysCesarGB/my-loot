097b1a9c42f712cc12f2f6ce776d9a3ecdeb6208
Tallys Cesar <tallycesardev@gmail.com>
2025-03-10 09:27:43 -0300

    remove unused assets and components; add SignIn and SignUp screens

---
+++ app/SingUp/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
+
+// SignIn.tsx
+
+export default function SingIn() {
+  return (
+    <View>
+      <Text>Sign In</Text>
+      <Text>
+        Don't have an account? <Link href="/">Sign In</Link>
+      </Text>
+    </View>
+  );
+}
